024b79a8fa270b436397a2532110763bcf85829b
UZIEL45 <uziel5551@gmail.com>
2020-12-04 01:34:47 -0600

    prac1 php

---
+++ prac1.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h1> estamos empezando a programar php ...</h1>";
+for ($i=1;$i<10;$i++){
+    echo "valor de numero $i <br>";
+}
+
+for ($i=1;$i<10;$i++){
+    echo 'input type="text" name="txtc'.$i.'" placeholder="casilla numero'.$i . '"><br>';
+}
+?>
